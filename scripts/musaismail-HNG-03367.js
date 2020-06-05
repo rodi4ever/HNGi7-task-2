ba5a6ea03d71e832d6b813006c7c3e34c72a01c9
Rodii <40048835+rodi4ever@users.noreply.github.com>
2020-06-05 20:02:54 +0100

    Update musaismail-HNG-03367.js

--- scripts/musaismail-HNG-03367.js
+++ scripts/musaismail-HNG-03367.js
@@ -4,4 +4,4 @@ let id = "HNG-03367";
 let language = "Javascript";
 let email ="musaharold@yahoo.com";
 
-console.log("Hello all, this is  " + fullName + " with HNG ID " + ID + " and email " + email + " with " + language + " into stage 2 task");
+console.log("Hello world, this is  " + fullName + " with HNG ID " + id + " and email " + email + " with " + language + " into stage 2 task");
